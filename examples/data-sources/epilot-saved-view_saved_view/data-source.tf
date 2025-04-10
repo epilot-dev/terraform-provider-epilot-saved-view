98596ca01b23654c02629a6fdcd75a0026e1af51
Nishu Goel <n.goel@epilot.cloud>
2025-04-11 01:43:59 +0530

    🎉⭐️

---
+++ examples/data-sources/epilot-saved-view_saved_view/data-source.tf
@@ -0,0 +1,3 @@
+data "epilot-saved-view_saved_view" "my_savedview" {
+  id = "cdef8248-63d3-4c71-98e4-c0e5c16966e1"
+}
---
+++ examples/provider/provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    epilot-saved-view = {
+      source  = "epilot-dev/epilot-saved-view"
+      version = "0.10.3"
+    }
+  }
+}
+
+provider "epilot-saved-view" {
+  # Configuration options
+}
